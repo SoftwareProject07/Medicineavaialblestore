e87dee8548c602daabc55cc445b374db37d90a6f
GAUTAM DEV <coolgautamshrivastav@gmail.com>
2025-12-16 22:28:37 +0530

    api_integrate

--- medicineonlineservices/src/Component/Registeration.jsx
+++ medicineonlineservices/src/Component/Registeration.jsx
@@ -1,8 +1,8 @@
 import React, { Fragment, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./styles/registerations.css";
-//import axios from "axios";
-import API from "../Services/API";
+//import API from "../Services/API";
 
 export default function Registeration() {
   const navigate = useNavigate();
@@ -29,138 +29,104 @@ export default function Registeration() {
   //     Type: type,
   //     CreateOn: createon
   //   };
-const handleSave = async () => {
-  const data = {
-    FirstName: firstname,
-    MiddleName: middlename,
-    LastName: lastname,
-    Password: password,
-    Email: email,
-    Fund: found,
-    Type: type,
-    CreateOn: createon,
-  };
 
-  //   axios
-  //     .post(
-  //       "http://localhost:5256/api/USERMEDICINE/CREATERegisterUser",
-  //       data,
-  //       { headers: { "Content-Type": "application/json" } }
-  //     )
-  //     .then((response) => {
-  //       alert("Registration Successful!");
-  //       navigate("/login");
-  //     })
-  //     .catch((error) => {
-  //       if (error.response?.data) {
-  //         alert(JSON.stringify(error.response.data));
-  //       } else {
-  //         alert("Server error");
-  //       }
-  //     });
-  // };
+  // ✅ FINAL SAVE FUNCTION
+  const handleSave = async () => {
+    const data = {
+      FirstName: firstname,
+      MiddleName: middlename,
+      LastName: lastname,
+      Password: password,
+      Email: email,
+      Fund: found,
+      Type: type,
+      CreateOn: createon,
+    };
 
     try {
-    const res = await API.post(
-      "/api/USERMEDICINE/CREATERegisterUser",
-      data
-    );
+      await axios.post(
+        "https://ecommerencesite-api.onrender.com/api/USERMEDICINE/CREATERegisterUser",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-    alert(res.data.message);
-  } catch (err) {
-    console.error(err);
-    alert("API Error");
-  }
-};
+      alert("Registration Successful");
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+      alert("API Error");
+    }
+  };
 
   return (
     <Fragment>
       <div className="bg">
-        <section
-          className="vh-100 bg-image"
-          // style={{
-          //   backgroundImage:
-          //     "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')"
-          // }}
-        >
-          <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-            <div className="container h-100">
-              <div className="row d-flex justify-content-center align-items-center h-100">
-                <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-                  <div className="card" style={{ borderRadius: "15px" }}>
-                    <div className="card-body p-5">
-                      <h2 className="text-uppercase text-center mb-5">
-                        Create an account
-                      </h2>
+        {/* ❌ YAHAN // COMMENT NAHI LAGANA */}
+        <section className="vh-100">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-8 col-lg-6">
+                <div className="card p-4" style={{ borderRadius: "15px" }}>
+                  <h3 className="text-center mb-4">Create an account</h3>
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label">First Name</label>
-                        <input
-                          type="text"
-                          className="form-control form-control-lg"
-                          value={firstname}
-                          onChange={(e) => setFirstName(e.target.value)}
-                        />
-                      </div>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="First Name"
+                    value={firstname}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label">Middle Name</label>
-                        <input
-                          type="text"
-                          className="form-control form-control-lg"
-                          value={middlename}
-                          onChange={(e) => setMiddleName(e.target.value)}
-                        />
-                      </div>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Middle Name"
+                    value={middlename}
+                    onChange={(e) => setMiddleName(e.target.value)}
+                  />
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label">Last Name</label>
-                        <input
-                          type="text"
-                          className="form-control form-control-lg"
-                          value={lastname}
-                          onChange={(e) => setLastName(e.target.value)}
-                        />
-                      </div>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Last Name"
+                    value={lastname}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label">Password</label>
-                        <input
-                          type="password"
-                          className="form-control form-control-lg"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                      </div>
+                  <input
+                    type="password"
+                    className="form-control mb-2"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label">Email</label>
-                        <input
-                          type="email"
-                          className="form-control form-control-lg"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
-                      </div>
+                  <input
+                    type="email"
+                    className="form-control mb-3"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
 
-                      <div className="d-flex justify-content-center">
-                        <button
-                          type="button"
-                          className="btn btn-success btn-lg"
-                          onClick={handleSave}
-                        >
-                          Register
-                        </button>
-                      </div>
+                  <button
+                    type="button"
+                    className="btn btn-success w-100"
+                    onClick={handleSave}
+                  >
+                    Register
+                  </button>
 
-                      <p className="text-center text-muted mt-4">
-                        Have already an account?{" "}
-                        <Link to="/login">
-                          <u>Login here</u>
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
+                  <p className="text-center mt-3">
+                    Already have an account?{" "}
+                    <Link to="/login">
+                      <u>Login here</u>
+                    </Link>
+                  </p>
                 </div>
               </div>
             </div>
---
+++ medicineonlineservices/src/Services/API.jsx
@@ -0,0 +1,21 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default API;
+
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default API;
