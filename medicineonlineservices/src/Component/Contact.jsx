fdb3152f72d215f172ff95486be608c28008f350
GAUTAM DEV <coolgautamshrivastav@gmail.com>
2025-12-18 16:53:24 +0530

    tryudpatesoon

--- medicineonlineservices/src/Component/Contact.jsx
+++ medicineonlineservices/src/Component/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
 
 <>
     <div className="conactdesgin">
-
+{/* dmddmd */}
        <h2>AVAILABLE TIMING:24*7</h2>
         <h2>Contact Person:7033132629</h2>
         <h1>CURRENT ADDRESS :2nd floor, 206 flat, JS ROOP HOMES NEAR BY VIHAR HERITAGE SECTOR-1,GAUTAM BUDDH NAGAR  GREATENOIDA EXTENSION </h1>
